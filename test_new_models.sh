5986e9d24f21aa63936a49ae8d19ecfa8a96adb0
Marie Haahr Petersen <marie.haahr.petersen@gmail.com>
2026-02-25 16:43:36 +0100

    testing large gemma

--- test_new_models.sh
+++ test_new_models.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=gemma-3b-gguf-testrun
+#SBATCH --job-name=gemma3-27b-test
+#SBATCH --account=researchers
 #SBATCH --partition=scavenge
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=24G  
-#SBATCH --time=04:00:00
+#SBATCH --gres=gpu
+#SBATCH --mem=24G
+#SBATCH --time=02:00:00
+#SBATCH --constraint="gpu_h100|gpu_a100_80gb|gpu_a100_40gb"
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=BEGIN,END
-
-nvidia-smi --query-gpu=gpu_name,pstate,timestamp,utilization.gpu,utilization.memory,memory.total,memory.used --format=csv -l 1 -f nvidia-smi.log &
 
 
 echo "Host: $(hostname)"
@@ -20,8 +19,8 @@ nvidia-smi
 # rm -rf .venv
 # uv venv
 
-uv sync 
-# uv run run-eval.py --model_name llama-3.1-8b -limit 20 --outdir results/ --repetition 1 #virker ikke 
+uv sync
+# uv run run-eval.py --model_name llama-3.1-8b -limit 20 --outdir results/ --repetition 1 #virker ikke
 
 # uv run run-eval.py --model_name llama-3.2-1b --outdir results_extra/ --repetition 10 --dataset_path data/sarc/sarcasm2_llama.csv
 
@@ -37,8 +36,7 @@ uv sync
 #uv run test.py --model_name qwen-2.5-1.5b --repetition 3 --round 1 --batch_size 2 -limit 6
 
 # new models
-uv run test.py --model_name gemma-3-4b --repetition 5 --round 1 --batch_size 5 -limit 5
+# uv run test.py --model_name gemma-3-4b --repetition 5 --round 1 --batch_size 5 -limit 25
 # uv run test.py --model_name gpt-oss-9b --repetition 5 --round 1 --batch_size 5 -limit 5
 # uv run test.py --model_name gpt-20b --repetition 5 --round 1 --batch_size 5 -limit 5
-# uv run test.py --model_name gemma-3-27b --repetition 5 --round 1 --batch_size 5 -limit 5
-
+uv run test.py --model_name gemma-3-27b --repetition 5 --round 1 --batch_size 5 -limit 25
