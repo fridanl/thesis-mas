e59c85ea569bfe5d0618f3e3d344a2fd80f27d3a
Marie Haahr Petersen <mhpe@slurmhead.hpc.itu.dk>
2026-02-26 14:54:55 +0100

    res for debugging

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
 #SBATCH --time=04:00:00
-#SBATCH --constraint="gpu_a100_40gb"
+#SBATCH --constraint="gpu_a100_40gb|gpu_a100_80gb|gpu_l40s|gpu_a30"
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=BEGIN,END
 
