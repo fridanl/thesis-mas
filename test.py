af538a5e7c33f56957997f200feeae412b255bca
fridanl <frida.nl@hotmail.com>
2026-02-26 10:07:17 +0100

    print

--- test.py
+++ test.py
@@ -91,6 +91,7 @@ def main(args):
             history=spec.history,
             round=spec.round)
 
+        print(conversations[0][0])
         logger.debug('Running inference...')
         start_time = time.time()
         raw_outputs, parsed = run_inference(llm, conversations=conversations, sampling=sampling, output_model=spec.output_model)
