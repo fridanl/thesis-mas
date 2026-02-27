a043747a2da1001d3bc4dbbff7b3ccf91f2e56b3
fridanl <frida.nl@hotmail.com>
2026-02-27 14:13:21 +0100

    test run for hpc

--- test.sh
+++ test.sh
@@ -20,4 +20,7 @@ uv sync
 
 SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 
-uv run test.py --model_name llama-3.1-8b --repetition 10 --round 1 --batch_size 1 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+
+uv run test.py --model_name llama-3.1-8b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+
+uv run test.py --model_name gemma-3-4b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
