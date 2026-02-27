cc239375a622f6d79aa1d269fbb2c16bc954cf0e
Marie Haahr Petersen <marie.haahr.petersen@gmail.com>
2026-02-27 16:19:07 +0100

    made the jobfile ready and fixed a mistake in the models.yaml file

--- test.sh
+++ test.sh
@@ -20,16 +20,21 @@ uv sync
 
 SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 
-
+#tjek
 # uv run test.py --model_name llama-3.1-8b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
-#uv run test.py --model_name gemma-3-4b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+# tjek
+# uv run test.py --model_name gemma-3-4b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
-#uv run test.py --model_name gemma-3-27b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+# mangler
+# uv run test.py --model_name gemma-3-27b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
-#uv run test.py --model_name llama-3.3-70b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+# mangler
+# uv run test.py --model_name llama-3.3-70b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
-uv run test.py --model_name qwen-2.5-7b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+# mangler
+# uv run test.py --model_name qwen-2.5-7b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
+# mangler
 #uv run test.py --model_name qwen-2.5-72b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
