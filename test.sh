1e30248b92c3f90a5dfbb0659c2ae87c6c5ca816
fridanl <frida.nl@hotmail.com>
2026-02-28 12:19:59 +0100

    run with large batch

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=scavenge
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=40G
+#SBATCH --mem=80G
 #SBATCH --time=04:00:00
-#SBATCH --constraint="gpu_a100_40gb|gpu_a100_80gb|gpu_l40s|gpu_a30"
+#SBATCH --constraint="gpu_a100_80gb|gpu_h100"
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=BEGIN,END
 
@@ -30,10 +30,10 @@ SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 # uv run log_run.py --model_name gemma-3-27b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
 # mangler
-# uv run log_run.py --model_name llama-3.3-70b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+uv run run.py --model_name llama-3.3-70b --repetition 2 --round 1 --batch_size 256 -limit 300 --slurm_output "${SLURM_OUTPUT_FILE}"
 
 # mangler
-uv run log_run.py --model_name qwen-2.5-7b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
+#uv run log_run.py --model_name qwen-2.5-7b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
 
 # mangler
 #uv run log_run.py --model_name qwen-2.5-72b --repetition 10 --round 1 --batch_size 20 -limit 30 --slurm_output "${SLURM_OUTPUT_FILE}"
