04e5e8426ffc7ebfe46b39e19cae9b4b307adf15
Marie Haahr Petersen <mhpe@slurmhead.hpc.itu.dk>
2026-03-05 14:10:30 +0100

    moved out the results to see it better, so it gets pushed

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=round2-test-gemma
+#SBATCH --job-name=test-failed-runs
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80G
-#SBATCH --time=24:00:00
+#SBATCH --time=3:00:00
 #SBATCH --constraint="gpu_a100_80gb|gpu_h100|gpu_l40s|gpu_a30"
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=BEGIN,END
@@ -18,7 +18,7 @@ nvidia-smi
 
 uv sync 
 
-# SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
+SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 
 # done
 # uv run run.py --model_name llama-3.1-8b --repetition 10 --round 1 --batch_size 256 --slurm_output "${SLURM_OUTPUT_FILE}"
@@ -33,5 +33,8 @@ uv sync
 # uv run run.py --model_name llama-3.3-70b --repetition 10 --round 1 --batch_size 256 --slurm_output "${SLURM_OUTPUT_FILE}"
 
 # round 2 test
-uv run run.py --model_name gemma-3-4b --repetition 1 --round 2 --batch_size 10 -limit 10 --history --dataset_path test_data.csv --slurm_output "${SLURM_OUTPUT_FILE}"
+# uv run run.py --model_name gemma-3-4b --repetition 1 --round 2 --batch_size 10 -limit 10 --history --dataset_path test_data.csv --slurm_output "${SLURM_OUTPUT_FILE}"
 
+
+# run failed examples 
+uv run run.py --model_name qwen-2.5-7b --repetition 1 --round 1 --batch_size 256 --dataset_path results/first/qwen-2.5-7b-sarcasm-failed.csv --slurm_output "${SLURM_OUTPUT_FILE}"
