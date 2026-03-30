530d32655760895fa4ab7ae003b090781c0b5a78
fridanl <fril@itu.dk>
2026-03-30 10:48:24 +0200

    small changes to check

--- check.py
+++ check.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 direc = "/home/rp-fril-mhpe/subsampled_input_round2"
 direc = Path(direc)
-files = list(direc.glob(f'*.csv'))
+files = list(direc.glob(f"*.csv"))
 
 for i, file in enumerate(files):
-    print(f'looking at file {i}: {file}')
+    print(f"looking at file {i}: {file}")
     df = pd.read_csv(file)
-    
-    print(f'this is the length of the df: {len(df)}')
-    grouped = df.groupby("match_type")["id"].count()
+
+    print(f"this is the length of the df: {df.shape}")
+    grouped = df.groupby("match_type")["id"].size().reset_index()
     print(grouped)
