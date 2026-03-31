899ec63602b8927933d758fc7a9ba526dd5e1892
fridanl <frida.nl@hotmail.com>
2026-03-31 15:16:41 +0200

    small changes to job file

--- round2.sh
+++ round2.sh
@@ -23,4 +23,5 @@ SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 # ROUND 2
 # model_name is the receiver, set the repetition to 1, set batch size to 256, enable --history, change the dataset path! and the outdir to: /home/rp-fril-mhpe/results-round2
 # uv run run.py --model_name gemma-3-4b --repetition 1 --round 2 --batch_size 256 --history --dataset_path test_data_round2.csv --slurm_output "${SLURM_OUTPUT_FILE}"
-uv run run.py --model_name gemma-3-4b --repetition 1 --round 2 --batch_size 256 --history --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/gemma-3-4b_disagree_subsampled.csv --outdir /home/rp-fril-mhpe/results-round2 --slurm_output "${SLURM_OUTPUT_FILE}"
+uv run run.py --model_name gemma-3-4b --repetition 1 --round 2 --batch_size 256 --history --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/gemma-3-4b_agree_subsampled.csv --outdir /home/rp-fril-mhpe  --slurm_output "${SLURM_OUTPUT_FILE}" -limit 10
+
