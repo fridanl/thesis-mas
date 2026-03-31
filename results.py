a057a965e93e0c87a47626affb524a72a4a1cee4
fridanl <fril@itu.dk>
2026-03-31 13:09:48 +0200

    checking 2nd round

--- results.py
+++ results.py
@@ -65,8 +65,6 @@ def main(args):
     profiles = profiles_root.get('profiles', {})
     models = list(profiles.keys())
 
-    models =  ["gpt-oss-20b", "llama-3.3-70b"] 
-
     first_round = load_first_round_results(base, models, args.dataset, failed = False)
     second_round_input = load_second_round_input(base, models, args.dataset, agreeing=False)
     second_round_input_subsampled = load_second_round_subsampled(base, models, args.dataset, agreeing=False)
@@ -81,29 +79,41 @@ def main(args):
         print(f'Number of claims after discarding claims with failed: {grouped[grouped['count'] == 10].shape[0]}')
 
 
-        print('SECOND ROUND')
-        second_round = second_round_input[model]
-        for sender in list(second_round['model_sender'].unique()):
-            if model == sender or sender not in models:
-                continue
-            print(f'SENDER: {sender}')
-            first_round_sender = first_round[sender]
-            first_round_receiver = first_round[model]
-            second_round_pair = second_round[second_round['model_sender'] == sender]
+        second = second_round_input[model]
+        second_grouped = second.groupby(['model_sender', 'match_type']).size().reset_index(name='count')
+        print('Second')
+        print(second_grouped)
 
-            print('Looking at 3 random samples')
-            candidates = list(second_round['id'].unique())
-            rands = random.sample(candidates, k=3)
-            for i in rands: 
-                print('First round results for receiver:\n')
-                print(first_round_receiver[first_round_receiver['id'] == i]['id', 'label'])
-                print('First round for sender:\n')
-                print(first_round_sender[first_round_sender['id'] == i]['id', 'label'])
+        second_sub = second_round_input_subsampled[model]
+        second_sub_grouped = second_sub.groupby(['model_sender', 'match_type']).size().reset_index(name='count')
+        print('Subsampled')
+        print(second_round_input_subsampled)
 
-                print(f'From the second round input for model: {model} as receiver')
+        # CODE FOR RANDOM CHECK OF CONSTRUCTED INPUT TO SECOND ROUND. 
+        # print('SECOND ROUND')
+        # second_round = second_round_input[model]
+        # for sender in list(second_round['model_sender'].unique()):
+        #     if model == sender or sender not in models:
+        #         continue
+        #     print(f'SENDER: {sender}')
+        #     first_round_sender = first_round[sender]
+        #     first_round_receiver = first_round[model]
+        #     second_round_pair = second_round[second_round['model_sender'] == sender]
 
-                print(second_round_pair[second_round_pair['id'] == i]['id', 'model_receiver', 'model_sender','label_receiver', 'label_sender','match_type'])
+        #     print('Looking at 3 random samples')
+        #     candidates = list(second_round['id'].unique())
+        #     rands = random.sample(candidates, k=3)
+        #     for i in rands: 
+        #         print('First round results for receiver:\n')
+        #         print(first_round_receiver[first_round_receiver['id'] == i]['id', 'label'])
+        #         print('First round for sender:\n')
+        #         print(first_round_sender[first_round_sender['id'] == i]['id', 'label'])
 
+        #         print(f'From the second round input for model: {model} as receiver')
+
+        #         print(second_round_pair[second_round_pair['id'] == i]['id', 'model_receiver', 'model_sender','label_receiver', 'label_sender','match_type'])
+
+    
 
 
 if __name__ == '__main__':
