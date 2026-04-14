cd6076ce0777b72f84b074e4df67bc806167e86e
fridanl <frida.nl@hotmail.com>
2026-04-14 11:42:02 +0200

    check self-interaction

--- second-round-input.sh
+++ second-round-input.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=second-round-input
+#SBATCH --job-name=second-round-input-self
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
-#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=80G
 #SBATCH --time=2:00:00
-#SBATCH --constraint="gpu_a100_80gb|gpu_h100|gpu_l40s|gpu_a30"
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=END
 
 echo "Host: $(hostname)"
 
 set -euo pipefail
-
-nvidia-smi
 
 #uv run -m src.make_second_round_input --dataset commonsense --output_root /home/rp-fril-mhpe/input_round2/commonsense
 uv run -m src.make_second_round_input --self_interaction True
