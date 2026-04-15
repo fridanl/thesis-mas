7805100807163757081c4b3b28e81ba35cbeae92
fridanl <fril@itu.dk>
2026-04-15 10:41:23 +0200

    ready for create self

--- second-round-input.sh
+++ second-round-input.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=second-round-input-self
+#SBATCH --job-name=second-round-input-self-common
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
 #SBATCH --cpus-per-task=4
@@ -12,4 +12,4 @@ echo "Host: $(hostname)"
 set -euo pipefail
 
 #uv run -m src.make_second_round_input --dataset commonsense --output_root /home/rp-fril-mhpe/input_round2/commonsense
-uv run -m src.make_second_round_input --self_interaction 
+uv run -m src.make_second_round_input --self_interaction --dataset commonsense
