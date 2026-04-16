5094981392a480640b3e88c865236ffec055941b
fridanl <fril@itu.dk>
2026-04-16 12:02:40 +0200

    qwen self

--- round2-self.sh
+++ round2-self.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=round2-self-sarcasm-small
+#SBATCH --job-name=round2-self-sarcasm-qwen-2.5-72b
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
-#SBATCH --gres=gpu:a30:1
+#SBATCH --gres=gpu:a100_80gb:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80G
 #SBATCH --time=06:00:00
@@ -68,15 +68,15 @@ SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 #  --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 # Disagree 
-uv run run.py \
-  --model_name gemma-3-4b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/gemma-3-4b_self_interaction_disagree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
+# uv run run.py \
+#   --model_name gemma-3-4b \
+#   --repetition 1 \
+#   --round 2 \
+#   --batch_size 256 \
+#   --history \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/gemma-3-4b_self_interaction_disagree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
+#   --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 
 # # gemma-3-27b
@@ -114,60 +114,73 @@ uv run run.py \
 
 # # llama-3.1-8b
 # agree
-uv run run.py \
-  --model_name llama-3.1-8b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/llama-3.1-8b_self_interaction_agree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
-
-uv run run.py \
-  --model_name llama-3.1-8b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/llama-3.1-8b_self_interaction_disagree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
-
-
-# # qwen-2.5-72b
 # uv run run.py \
-#   --model_name qwen-2.5-72b \
+#   --model_name llama-3.1-8b \
 #   --repetition 1 \
 #   --round 2 \
 #   --batch_size 256 \
 #   --history \
-#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/qwen-2.5-72b-self-interaction_subsampled.csv \
-#   --outdir /home/rp-fril-mhpe \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/llama-3.1-8b_self_interaction_agree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
 #   --slurm_output "${SLURM_OUTPUT_FILE}" 
+
+# uv run run.py \
+#   --model_name llama-3.1-8b \
+#   --repetition 1 \
+#   --round 2 \
+#   --batch_size 256 \
+#   --history \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/llama-3.1-8b_self_interaction_disagree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
+#   --slurm_output "${SLURM_OUTPUT_FILE}" 
+
+
+# # qwen-2.5-72b
+# agree 
+uv run run.py \
+  --model_name qwen-2.5-72b \
+  --repetition 1 \
+  --round 2 \
+  --batch_size 256 \
+  --history \
+  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/qwen-2.5-72b_self_interaction_agree_subsampled.csv \
+  --outdir /home/rp-fril-mhpe/self \
+  --slurm_output "${SLURM_OUTPUT_FILE}" 
+
+# disagree
+uv run run.py \
+  --model_name qwen-2.5-72b \
+  --repetition 1 \
+  --round 2 \
+  --batch_size 256 \
+  --history \
+  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/qwen-2.5-72b_self_interaction_disagree_subsampled.csv \
+  --outdir /home/rp-fril-mhpe/self \
+  --slurm_output "${SLURM_OUTPUT_FILE}" 
+
 
 # qwen-2.5-7b
 #agree 
-uv run run.py \
-  --model_name qwen-2.5-7b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/qwen-2.5-7b_self_interaction_agree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
+# uv run run.py \
+#   --model_name qwen-2.5-7b \
+#   --repetition 1 \
+#   --round 2 \
+#   --batch_size 256 \
+#   --history \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/qwen-2.5-7b_self_interaction_agree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
+#   --slurm_output "${SLURM_OUTPUT_FILE}" 
 
-#disagree 
-uv run run.py \
-  --model_name qwen-2.5-7b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/qwen-2.5-7b_self_interaction_disagree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
+# #disagree 
+# uv run run.py \
+#   --model_name qwen-2.5-7b \
+#   --repetition 1 \
+#   --round 2 \
+#   --batch_size 256 \
+#   --history \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/sarcasm/qwen-2.5-7b_self_interaction_disagree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
+#   --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 #-------------------------------------------------------------------------------------------------
 
