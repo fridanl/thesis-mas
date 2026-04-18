f6b46621ff1b5ba9d296607f1f01e72b570d02af
fridanl <fril@itu.dk>
2026-04-18 09:30:46 +0200

    big llama

--- round2-self.sh
+++ round2-self.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=round2-self-commonsense-small
+#SBATCH --job-name=round2-self-commonsense-llama-70b
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
-#SBATCH --gres=gpu:a30:1
+#SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80G
-#SBATCH --time=12:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=BEGIN,END
 
 echo "Host: $(hostname)"
-
-set -euo pipefail
 
 nvidia-smi
 
@@ -225,27 +223,27 @@ SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 
 # # llama-3.3-70b
 # agree 
-# uv run run.py \
-#   --model_name llama-3.3-70b \
-#   --repetition 1 \
-#   --round 2 \
-#   --batch_size 256 \
-#   --history \
-#   --dataset commonsense \
-#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/llama-3.3-70b_self_interaction_agree_subsampled.csv \
-#   --outdir /home/rp-fril-mhpe/self \
-#   --slurm_output "${SLURM_OUTPUT_FILE}" 
+uv run run.py \
+  --model_name llama-3.3-70b \
+  --repetition 1 \
+  --round 2 \
+  --batch_size 256 \
+  --history \
+  --dataset commonsense \
+  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/llama-3.3-70b_self_interaction_agree_subsampled.csv \
+  --outdir /home/rp-fril-mhpe/self \
+  --slurm_output "${SLURM_OUTPUT_FILE}" 
 # # #disagree
-# uv run run.py \
-#   --model_name llama-3.3-70b \
-#   --repetition 1 \
-#   --round 2 \
-#   --batch_size 256 \
-#   --history \
-#   --dataset commonsense \
-#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/llama-3.3-70b_self_interaction_disagree_subsampled.csv \
-#   --outdir /home/rp-fril-mhpe/self \
-#   --slurm_output "${SLURM_OUTPUT_FILE}" 
+uv run run.py \
+  --model_name llama-3.3-70b \
+  --repetition 1 \
+  --round 2 \
+  --batch_size 256 \
+  --history \
+  --dataset commonsense \
+  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/llama-3.3-70b_self_interaction_disagree_subsampled.csv \
+  --outdir /home/rp-fril-mhpe/self \
+  --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 # # llama-3.1-8b
 # agree
@@ -260,16 +258,16 @@ SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 #   --outdir /home/rp-fril-mhpe/self \
 #   --slurm_output "${SLURM_OUTPUT_FILE}" 
 
-uv run run.py \
-  --model_name llama-3.1-8b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset commonsense \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/llama-3.1-8b_self_interaction_disagree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
+# uv run run.py \
+#   --model_name llama-3.1-8b \
+#   --repetition 1 \
+#   --round 2 \
+#   --batch_size 256 \
+#   --history \
+#   --dataset commonsense \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/llama-3.1-8b_self_interaction_disagree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
+#   --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 
 # # qwen-2.5-72b
@@ -300,27 +298,27 @@ uv run run.py \
 
 # qwen-2.5-7b
 #agree 
-uv run run.py \
-  --model_name qwen-2.5-7b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset commonsense \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/qwen-2.5-7b_self_interaction_agree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
+# uv run run.py \
+#   --model_name qwen-2.5-7b \
+#   --repetition 1 \
+#   --round 2 \
+#   --batch_size 256 \
+#   --history \
+#   --dataset commonsense \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/qwen-2.5-7b_self_interaction_agree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
+#   --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 # #disagree 
-uv run run.py \
-  --model_name qwen-2.5-7b \
-  --repetition 1 \
-  --round 2 \
-  --batch_size 256 \
-  --history \
-  --dataset commonsense \
-  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/qwen-2.5-7b_self_interaction_disagree_subsampled.csv \
-  --outdir /home/rp-fril-mhpe/self \
-  --slurm_output "${SLURM_OUTPUT_FILE}" 
+# uv run run.py \
+#   --model_name qwen-2.5-7b \
+#   --repetition 1 \
+#   --round 2 \
+#   --batch_size 256 \
+#   --history \
+#   --dataset commonsense \
+#   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/commonsense/qwen-2.5-7b_self_interaction_disagree_subsampled.csv \
+#   --outdir /home/rp-fril-mhpe/self \
+#   --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 
