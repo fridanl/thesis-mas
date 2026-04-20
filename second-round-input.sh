20aaff0eed55bdc6cf07d00edebb0ce3631bb484
Marie Haahr Petersen <mhpe@slurmhead.hpc.itu.dk>
2026-04-20 22:33:53 +0200

    ready to run small models as receivers for sentiment

--- second-round-input.sh
+++ second-round-input.sh
@@ -11,5 +11,5 @@ echo "Host: $(hostname)"
 
 set -euo pipefail
 
-uv run -m src.make_second_round_input --dataset sentiment --output_root /home/rp-fril-mhpe/input_round2/commonsense
+uv run -m src.make_second_round_input --dataset sentiment --output_root /home/rp-fril-mhpe/input_round2
 #uv run -m src.make_second_round_input --self_interaction --dataset sentiment 
--- src/make_second_round_input.py
+++ src/make_second_round_input.py
@@ -298,8 +298,11 @@ def main(args):
     outdir = args.output_root 
     model_claim_dict, discard = load_and_preprocess(combined, t_config)
     if not args.self_interaction:
-        discard.to_csv(f'{outdir}/{args.dataset}/discarded.csv', index=False)                                  
-        print(f'Saving the discarded claims to {outdir}/{args.dataset}/discarded.csv')  
+        # TODO: uncomment
+        #discard.to_csv(f'{outdir}/{args.dataset}/discarded.csv', index=False)                                  
+        #TODO: comment
+        discard.to_csv(f'{outdir}/{args.dataset}/discarded1.csv', index=False)
+        print(f'Saving the discarded claims to {outdir}/{args.dataset}/discarded1.csv') # TODO: change  
 
     for receiver in model_names:
         if receiver not in model_claim_dict.keys():
