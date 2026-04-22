44b637c3379b64b0dd4232d7d217214c1c8485e6
Marie Haahr Petersen <marie.haahr.petersen@gmail.com>
2026-04-22 14:21:57 +0200

    ready to make self interaction for gpt for commonsense

--- second-round-input.sh
+++ second-round-input.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=r2-input-sarc-gpt
+#SBATCH --job-name=r2-input-com-gpt
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
 #SBATCH --cpus-per-task=4
@@ -15,8 +15,8 @@ set -euo pipefail
 
 
 # SARCASM SELF INTERACTION
-uv run -m src.make_second_round_input --self_interaction --dataset sarcasm --output_root /home/rp-fril-mhpe/tmp
+#uv run -m src.make_second_round_input --self_interaction --dataset sarcasm --output_root /home/rp-fril-mhpe/tmp
 
 
 # COMMONSENSE SELF INTERACTION
-#uv run -m src.make_second_round_input --self_interaction --dataset commonsense 
+uv run -m src.make_second_round_input --self_interaction --dataset commonsense --output_root /home/rp-fril-mhpe/tmp
