5f865a87e12c3dda40a16adc1c58927af55c0e24
fridanl <fril@itu.dk>
2026-04-29 10:54:20 +0200

    ready for robustness runs

--- robustness.sh
+++ robustness.sh
@@ -1,14 +1,13 @@
 #!/bin/bash 
-#SBATCH --job-name=test-llama-qwen-no-history
+#SBATCH --job-name=llama-qwen-robustness
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80G
-#SBATCH --time=01:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --gres=gpu:h100:1
-
 
 
 echo "Host: $(hostname)"
@@ -21,7 +20,7 @@ SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 
 # llama
 
-# NO HISTORY 
+# NO HISTORY, WITH EXPLANATION 
 uv run run.py \
   --model_name llama-3.3-70b \
   --repetition 1 \
@@ -30,8 +29,7 @@ uv run run.py \
   --dataset sarcasm \
   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/robustness/sarcasm/llama-3.3-70b_disagree_subsampled.csv \
   --outdir /home/rp-fril-mhpe/no_history \
-  --slurm_output "${SLURM_OUTPUT_FILE}" \
-  -limit 10
+  --slurm_output "${SLURM_OUTPUT_FILE}" 
 
 
 # NO EXPLANATION BUT WITH HISTORY
@@ -45,9 +43,34 @@ uv run run.py \
   --no_explanation \
   --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/robustness/sarcasm/llama-3.3-70b_disagree_subsampled.csv \
   --outdir /home/rp-fril-mhpe/no_explanation \
-  --slurm_output "${SLURM_OUTPUT_FILE}" \
-  -limit 10
+  --slurm_output "${SLURM_OUTPUT_FILE}" 
 
+
+
+# NO HISTORY, WITH EXPLANATION 
+uv run run.py \
+  --model_name qwen-2.5-72b \
+  --repetition 1 \
+  --round 2 \
+  --batch_size 256 \
+  --dataset sarcasm \
+  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/robustness/sarcasm/qwen-2.5-72b_disagree_subsampled.csv \
+  --outdir /home/rp-fril-mhpe/no_history \
+  --slurm_output "${SLURM_OUTPUT_FILE}" 
+
+
+# NO EXPLANATION BUT WITH HISTORY
+uv run run.py \
+  --model_name qwen-2.5-72b \
+  --repetition 1 \
+  --round 2 \
+  --batch_size 256 \
+  --dataset sarcasm \
+  --history \
+  --no_explanation \
+  --dataset_path /home/rp-fril-mhpe/subsampled_input_round2/robustness/sarcasm/qwen-2.5-72b_disagree_subsampled.csv \
+  --outdir /home/rp-fril-mhpe/no_explanation \
+  --slurm_output "${SLURM_OUTPUT_FILE}"
 
 
 
