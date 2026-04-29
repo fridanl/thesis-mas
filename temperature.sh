5ffded79c1b040c251e7f6f732d5e6d8dc14af4f
Marie Haahr Petersen <marie.haahr.petersen@gmail.com>
2026-04-29 11:24:09 +0200

    ready for round1 temperature

--- temperature.sh
+++ temperature.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=temperature-test1
+#SBATCH --job-name=temperature-llamahigh-r1
 #SBATCH --account=researchers
 #SBATCH --partition=scavenge
 #SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80G
-#SBATCH --time=24:00:00
+#SBATCH --time=15:00:00
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --mail-type=BEGIN,END
 
@@ -20,4 +20,24 @@ uv sync
 SLURM_OUTPUT_FILE="logs/${SLURM_JOB_NAME}.${SLURM_JOB_ID}.out"
 
 # tester
-uv run run.py --model_name tester --repetition 10 --round 1 --limit 20 --batch_size 256 --outdir /home/rp-fril-mhpe/tmp/temperature --slurm_output "${SLURM_OUTPUT_FILE}"
+#uv run run.py --model_name tester --repetition 10 --round 1 --limit 20 --batch_size 256 --outdir /home/rp-fril-mhpe/tmp/temperature --slurm_output "${SLURM_OUTPUT_FILE}"
+
+# ROUND 1
+# high llama
+uv run run.py --model_name llama-3.3-70b-high-temp --repetition 10 --round 1 --limit 50_000 --batch_size 256 --outdir /home/rp-fril-mhpe/temperature --slurm_output "${SLURM_OUTPUT_FILE}"
+# high qwen
+#uv run run.py --model_name qwen-2.5-72b-high-temp --repetition 10 --round 1 --limit 50_000 --batch_size 256 --outdir /home/rp-fril-mhpe/temperature --slurm_output "${SLURM_OUTPUT_FILE}"
+
+
+# low llama
+#uv run run.py --model_name llama-3.3-70b-low-temp --repetition 10 --round 1 --limit 50_000 --batch_size 256 --outdir /home/rp-fril-mhpe/temperature --slurm_output "${SLURM_OUTPUT_FILE}"
+# low qwen
+#uv run run.py --model_name qwen-2.5-72b-low-temp --repetition 10 --round 1 --limit 50_000 --batch_size 256 --outdir /home/rp-fril-mhpe/temperature --slurm_output "${SLURM_OUTPUT_FILE}"
+
+
+# ROUND 2
+# receiver default qwen (senders low and high llama in dataset)
+# uv run run.py --model_name qwen-2.5-72b --repetition 1 --round 2 --batch_size 256 --history --dataset_path /home/rp-fril-mhpe/temperature/subsampled_input_round2/sarcasm/gemma-3-4b_disagree_subsampled.csv --outdir /home/rp-fril-mhpe/temperature  --slurm_output "${SLURM_OUTPUT_FILE}" --dataset sentiment
+
+# receiver default llama (senders low and high qwen in dataset)
+# uv run run.py --model_name llama-3.3-70b --repetition 1 --round 2 --batch_size 256 --history --dataset_path /home/rp-fril-mhpe/temperature/subsampled_input_round2/sarcasm/gemma-3-4b_disagree_subsampled.csv --outdir /home/rp-fril-mhpe/temperature  --slurm_output "${SLURM_OUTPUT_FILE}" --dataset sentiment
