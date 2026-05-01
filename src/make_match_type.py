e83c980a9c629c123d2ce8b0c430ba8fea56d6ed
fridanl <fril@itu.dk>
2026-05-01 09:34:28 +0200

    match type agreeing cases

--- src/make_match_type.py
+++ src/make_match_type.py
@@ -6,11 +6,12 @@ def add_match_type_to_gpt(
     dataset: str = 'sarcasm',
     model: str = 'gpt-oss-20b',
     self_interaction: bool = False):
+
     # Load second round results
     if self_interaction:
-        second_path = base / 'self' / 'second' / f'{model}-{dataset}_no_match.csv'
+        second_path = base / 'self' / 'second' / f'{model}-{dataset}-self_interaction_agree_no_match.csv'
     else:
-        second_path = base / 'second' / f'{model}-{dataset}_no_match.csv'
+        second_path = base / 'second' / f'{model}-{dataset}-agree_no_match.csv'
 
     # second_path = base / 'second' / f'{model}-{dataset}_no_match.csv'
     second = pd.read_csv(second_path)
@@ -91,6 +92,34 @@ def add_match_type_to_gpt(
 
     return second
 
+
+def concat(df, self_interaction):
+    base = Path('/home/rp-fril-mhpe/')
+
+    # Load existing file
+    if not self_interaction:
+        existing_path = base / 'second' / 'gpt-oss-20b-sarcasm.csv'
+    else:
+        existing_path = base / 'self' / 'second' / 'gpt-oss-20b-sarcasm.csv' 
+
+    existing_df = pd.read_csv(existing_path)
+    print(f'Rows in original df: {len(existing_df)}')
+
+    # Match type df 
+    print(f'Rows in match_type df: {len(df)}')
+
+    # Concat and save
+    combined_df = pd.concat([existing_df, df], ignore_index=True)
+    print(f'Rows in combined df: {len(combined_df)}')
+
+    if not self_interaction:
+        output_path = base / 'second' / 'gpt-oss-20b-sarcasm_new.csv'
+    else:
+        existing_path = base / 'self' / 'second' / 'gpt-oss-20b-sarcasm_new.csv' 
+    
+    combined_df.to_csv(output_path, index=False)
+    print(f'Saving combined file to: {output_path}')
+
 def make_qwen_llama_data():
     print('Constructing dataset for llama as receiver...')
     df = pd.read_csv('/home/rp-fril-mhpe/subsampled_input_round2/sarcasm/llama-3.3-70b_disagree_subsampled.csv')
@@ -129,13 +158,26 @@ if __name__ == '__main__':
 
 
     # gpt sentiment self
+    # base = Path('/home/rp-fril-mhpe/')
+    # second_self = add_match_type_to_gpt(base, dataset='sentiment', self_interaction=True)
+    # second_self.to_csv(base / 'self' / 'second' / 'gpt-oss-20b-sentiment.csv', index=False) 
+    # print(f'Saving file gpt self sentiment file to: {base}/self/second/gpt-oss-20b-sentiment.csv')
+
+
+    # # gpt sentiment
+    # second = add_match_type_to_gpt(base, dataset='sentiment', self_interaction=False)
+    # second.to_csv(base / 'second' / 'gpt-oss-20b-sentiment.csv', index=False) 
+    # print(f'Saving file gpt sentiment file to: {base}/second/gpt-oss-20b-sentiment.csv')
+
+    #gpt agreeing
     base = Path('/home/rp-fril-mhpe/')
-    second_self = add_match_type_to_gpt(base, dataset='sentiment', self_interaction=True)
-    second_self.to_csv(base / 'self' / 'second' / 'gpt-oss-20b-sentiment.csv', index=False) 
-    print(f'Saving file gpt self sentiment file to: {base}/self/second/gpt-oss-20b-sentiment.csv')
+    second = add_match_type_to_gpt(base, dataset='sarcasm', self_interaction=False)
+    second.to_csv(base / 'second' / 'gpt-oss-20b-sarcasm_agree.csv', index=False) 
+    print(f'Saving file gpt sentiment file to: {base}/second/gpt-oss-20b-sarcasm_agree.csv')
+    concat(second, self_interaction=False)
 
-
-    # gpt sentiment
-    second = add_match_type_to_gpt(base, dataset='sentiment', self_interaction=False)
-    second.to_csv(base / 'second' / 'gpt-oss-20b-sentiment.csv', index=False) 
-    print(f'Saving file gpt sentiment file to: {base}/second/gpt-oss-20b-sentiment.csv')
+    # gpt agreeing, self-interaction 
+    second_self = add_match_type_to_gpt(base, dataset='sarcasm', self_interaction=True)
+    second_self.to_csv(base / 'self' / 'second' / 'gpt-oss-20b-sarcasm_agree.csv', index=False) 
+    print(f'Saving file gpt sentiment file to: {base}/self/second/gpt-oss-20b-sarcasm_agree.csv')
+    concat(second_self, self_interaction=True)
